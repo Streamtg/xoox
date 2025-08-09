a13eadb599aa0ad2add73b5cc1e9d34b1b1fce7d
Streamtg <202310475@issu.edu.do>
2025-08-09 01:49:24 -0400

    Update database.py

--- FileStream/utils/database.py
+++ FileStream/utils/database.py
@@ -1,151 +1,139 @@
+import pymongo
 import time
-import json
-import redis.asyncio as redis
+import motor.motor_asyncio
+from bson.objectid import ObjectId
+from bson.errors import InvalidId
 from FileStream.server.exceptions import FIleNotFound
 
 class Database:
-    def __init__(self, redis_url):
-        # Conexión asíncrona a Redis
-        self.redis = redis.from_url(redis_url, decode_responses=True)
+    def __init__(self, uri, database_name):
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        self.db = self._client[database_name]
+        self.col = self.db.users
+        self.black = self.db.blacklist
+        self.file = self.db.file
 
     #---------------------[ NEW USER ]---------------------#
-    def new_user(self, user_id):
-        return {
-            "id": user_id,
-            "join_date": time.time(),
-            "Links": 0,
-            "Plan": "Free"  # Default plan
-        }
+    def new_user(self, id):
+        return dict(
+            id=id,
+            join_date=time.time(),
+            Links=0
+        )
 
     #---------------------[ ADD USER ]---------------------#
-    async def add_user(self, user_id):
-        exists = await self.redis.exists(f"user:{user_id}")
-        if not exists:
-            user = self.new_user(user_id)
-            await self.redis.set(f"user:{user_id}", json.dumps(user))
+    async def add_user(self, id):
+        user = self.new_user(id)
+        await self.col.insert_one(user)
 
     #---------------------[ GET USER ]---------------------#
-    async def get_user(self, user_id):
-        data = await self.redis.get(f"user:{user_id}")
-        if data:
-            return json.loads(data)
-        return None
+    async def get_user(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user
 
-    #---------------------[ TOTAL USERS COUNT ]---------------------#
+    #---------------------[ CHECK USER ]---------------------#
     async def total_users_count(self):
-        keys = await self.redis.keys("user:*")
-        return len(keys)
+        count = await self.col.count_documents({})
+        return count
 
-    #---------------------[ GET ALL USERS ]---------------------#
     async def get_all_users(self):
-        keys = await self.redis.keys("user:*")
-        users = []
-        for key in keys:
-            data = await self.redis.get(key)
-            if data:
-                users.append(json.loads(data))
-        return users
+        all_users = self.col.find({})
+        return all_users
 
-    #---------------------[ DELETE USER ]---------------------#
+    #---------------------[ REMOVE USER ]---------------------#
     async def delete_user(self, user_id):
-        await self.redis.delete(f"user:{user_id}")
+        await self.col.delete_many({'id': int(user_id)})
 
     #---------------------[ BAN, UNBAN USER ]---------------------#
-    async def ban_user(self, user_id):
-        # Ban stored as key "ban:<user_id>"
-        await self.redis.set(f"ban:{user_id}", time.time())
+    def black_user(self, id):
+        return dict(
+            id=id,
+            ban_date=time.time()
+        )
 
-    async def unban_user(self, user_id):
-        await self.redis.delete(f"ban:{user_id}")
+    async def ban_user(self, id):
+        user = self.black_user(id)
+        await self.black.insert_one(user)
 
-    async def is_user_banned(self, user_id):
-        exists = await self.redis.exists(f"ban:{user_id}")
-        return exists == 1
+    async def unban_user(self, id):
+        await self.black.delete_one({'id': int(id)})
+
+    async def is_user_banned(self, id):
+        user = await self.black.find_one({'id': int(id)})
+        return True if user else False
 
     async def total_banned_users_count(self):
-        keys = await self.redis.keys("ban:*")
-        return len(keys)
+        count = await self.black.count_documents({})
+        return count
 
-    #---------------------[ ADD FILE ]---------------------#
+    #---------------------[ ADD FILE TO DB ]---------------------#
     async def add_file(self, file_info):
-        file_id = file_info["_id"]  # Asigna tú mismo el _id antes de llamar
-        file_key = f"file:{file_id}"
-
-        exists = await self.redis.exists(file_key)
-        if exists:
-            return file_id  # Ya existe
-
         file_info["time"] = time.time()
-        await self.increment_links(file_info["user_id"], 1)
-        await self.redis.set(file_key, json.dumps(file_info))
-        return file_id
+        fetch_old = await self.get_file_by_fileuniqueid(file_info["user_id"], file_info["file_unique_id"])
+        if fetch_old:
+            return fetch_old["_id"]
+        await self.count_links(file_info["user_id"], "+")
+        return (await self.file.insert_one(file_info)).inserted_id
 
-    #---------------------[ FIND FILES ]---------------------#
-    async def find_files(self, user_id, range_):
-        # Redis no es una base de datos con consultas complejas,
-        # por lo que guardaremos listas de IDs por usuario para paginar.
-        start, end = range_[0]-1, range_[1]
-        user_files_key = f"userfiles:{user_id}"
-        file_ids = await self.redis.lrange(user_files_key, start, end-1)
-        files = []
-        for fid in file_ids:
-            data = await self.redis.get(f"file:{fid}")
-            if data:
-                files.append(json.loads(data))
-        total_files = await self.redis.llen(user_files_key)
-        return files, total_files
+    #---------------------[ FIND FILE IN DB ]---------------------#
+    async def find_files(self, user_id, range):
+        user_files = self.file.find({"user_id": user_id})
+        user_files = user_files.skip(range[0] - 1)
+        user_files = user_files.limit(range[1] - range[0] + 1)
+        user_files = user_files.sort('_id', pymongo.DESCENDING)
+        total_files = await self.file.count_documents({"user_id": user_id})
+        return user_files, total_files
 
-    #---------------------[ GET FILE ]---------------------#
-    async def get_file(self, file_id):
-        data = await self.redis.get(f"file:{file_id}")
-        if data:
-            return json.loads(data)
-        raise FIleNotFound
+    async def get_file(self, _id):
+        try:
+            file_info = await self.file.find_one({"_id": ObjectId(_id)})
+            if not file_info:
+                raise FIleNotFound
+            return file_info
+        except InvalidId:
+            raise FIleNotFound
+
+    async def get_file_by_fileuniqueid(self, id, file_unique_id, many=False):
+        if many:
+            return self.file.find({"file_unique_id": file_unique_id})
+        else:
+            file_info = await self.file.find_one({"user_id": id, "file_unique_id": file_unique_id})
+            if file_info:
+                return file_info
+        return False
 
     #---------------------[ TOTAL FILES ]---------------------#
-    async def total_files(self, user_id=None):
-        if user_id:
-            return await self.redis.llen(f"userfiles:{user_id}")
-        else:
-            keys = await self.redis.keys("file:*")
-            return len(keys)
+    async def total_files(self, id=None):
+        if id:
+            return await self.file.count_documents({"user_id": id})
+        return await self.file.count_documents({})
 
-    #---------------------[ DELETE FILE ]---------------------#
-    async def delete_one_file(self, file_id):
-        file_key = f"file:{file_id}"
-        data = await self.redis.get(file_key)
-        if data:
-            file_info = json.loads(data)
-            await self.redis.delete(file_key)
-            # Además, quita este file_id de la lista userfiles
-            await self.redis.lrem(f"userfiles:{file_info['user_id']}", 0, file_id)
-            await self.increment_links(file_info['user_id'], -1)
+    #---------------------[ DELETE FILES ]---------------------#
+    async def delete_one_file(self, _id):
+        await self.file.delete_one({'_id': ObjectId(_id)})
 
-    #---------------------[ UPDATE FILE IDS ]---------------------#
-    async def update_file_ids(self, file_id, file_ids: dict):
-        data = await self.redis.get(f"file:{file_id}")
-        if data:
-            file_info = json.loads(data)
-            file_info["file_ids"] = file_ids
-            await self.redis.set(f"file:{file_id}", json.dumps(file_info))
+    #---------------------[ UPDATE FILES ]---------------------#
+    async def update_file_ids(self, _id, file_ids: dict):
+        await self.file.update_one({"_id": ObjectId(_id)}, {"$set": {"file_ids": file_ids}})
 
-    #---------------------[ LINK AVAILABLE (Plan) ]---------------------#
-    async def link_available(self, user_id):
-        user = await self.get_user(user_id)
-        if not user:
+    #---------------------[ PAID SYS ]---------------------#
+    async def link_available(self, id):
+        user = await self.col.find_one({"id": id})
+
+        if user is None:
             return False
+
         if user.get("Plan") == "Plus":
             return "Plus"
+
         elif user.get("Plan") == "Free":
-            files = await self.total_files(user_id)
+            files = await self.file.count_documents({"user_id": id})
             if files < 11:
                 return True
         return False
 
-    #---------------------[ COUNT LINKS ]---------------------#
-    async def increment_links(self, user_id, delta: int):
-        # Actualiza el contador Links en el usuario almacenado en Redis
-        user = await self.get_user(user_id)
-        if user:
-            user["Links"] = user.get("Links", 0) + delta
-            await self.redis.set(f"user:{user_id}", json.dumps(user))
+    async def count_links(self, id, operation: str):
+        if operation == "-":
+            await self.col.update_one({"id": id}, {"$inc": {"Links": -1}})
+        elif operation == "+":
+            await self.col.update_one({"id": id}, {"$inc": {"Links": 1}})
