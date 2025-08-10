6b430b29c7314bad2e949571c152450e42b3e99d
Streamtg <202310475@issu.edu.do>
2025-08-10 14:25:28 -0400

    Update __init__.py

--- FileStream/utils/__init__.py
+++ FileStream/utils/__init__.py
@@ -1,3 +1,14 @@
+from FileStream.config import Telegram
+from .database import Database
 from .time_format import get_readable_time
 from .file_properties import get_name, get_file_ids
 from .custom_dl import ByteStreamer
+import asyncio
+
+db = Database(Telegram.REDIS_URL, Telegram.SESSION_NAME)
+
+async def init_db():
+    await db.connect()
+
+def run_init_db_sync():
+    asyncio.run(init_db())
